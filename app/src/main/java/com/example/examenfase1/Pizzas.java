fd69ddff8db8a735debb0652b798d14660fdd018
junkdog505 <junkdog505@gmail.com>
2023-04-20 03:31:06 -0500

    Creación de las vistas

---
+++ app/src/main/java/com/example/examenfase1/Pizzas.java
@@ -0,0 +1,14 @@
+package com.example.examenfase1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Pizzas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pizzas);
+    }
+}
